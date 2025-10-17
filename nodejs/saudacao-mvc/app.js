05062c6470318003d84fa1040e3ac84e973d2ead
Ana Beatriz Macedo Guimarães <biabiabiabiaa2000@gmail.com>
2025-10-17 15:58:15 -0300

    versao nova

---
+++ nodejs/saudacao-mvc/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Middleware para arquivos estáticos
+app.use(express.static('public'));
+
+// Middleware para ler dados de formulários
+app.use(express.urlencoded({ extended: true }));
+
+// Rotas
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+
+const usersRoutes = require('./routes/index');
+app.use('/', usersRoutes);
+
+
+// Servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
